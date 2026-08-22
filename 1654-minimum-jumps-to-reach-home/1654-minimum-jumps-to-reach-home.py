cad860c9fb483a6e505eeb1865e4ef3a5c65e16f
Arish Muthaiah.K J <aswinharish_bcs27@mepcoeng.ac.in>
2026-08-22 18:45:36 +0530

    Time: 32 ms (34.11%), Space: 33 MB (5.14%) - LeetHub

---
+++ 1654-minimum-jumps-to-reach-home/1654-minimum-jumps-to-reach-home.py
@@ -0,0 +1,32 @@
+class Solution:
+
+    def minimumJumps(self, forbidden: List[int], a: int, b: int, x: int) -> int:
+
+        arr = set(forbidden)
+        counts = [9999999]
+
+        visited = set()
+
+        limit = max(max(forbidden, default=0), x) + a + b
+
+        def fn(idx, state, count):
+            if idx < 0 or idx > limit:
+                return
+            if idx == x:
+                counts[0] = min(counts[0], count)
+                return
+            if count >= counts[0]:
+                return
+            if (idx, state) in visited:
+                return
+            visited.add((idx, state))
+            if idx + a <= limit and idx + a not in arr:
+                fn(idx + a, True, count + 1)
+            if state and idx - b >= 0 and idx - b not in arr:
+                fn(idx - b, False, count + 1)
+        fn(0, True, 0)
+
+        if counts[0] == 9999999:
+            return -1
+
+        return counts[0]
