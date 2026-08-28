34ff85fe4103613a3ccd449e6190c42aebd3886d
Arish Muthaiah.K J <aswinharish_bcs27@mepcoeng.ac.in>
2026-08-28 20:43:30 +0530

    Time: 39 ms (18.83%), Space: 42.7 MB (82.43%) - LeetHub

---
+++ 2640-find-the-score-of-all-prefixes-of-an-array/2640-find-the-score-of-all-prefixes-of-an-array.py
@@ -0,0 +1,17 @@
+class Solution:
+    def findPrefixScore(self, nums: List[int]) -> List[int]:
+        prefix=[0]*len(nums)
+        prefix[0]=nums[0]
+        print(prefix)
+        ans=0
+        res=[]
+        for i in range(1,len(nums)):
+            prefix[i]=max(prefix[i-1],nums[i])
+        print(res)
+        res=[]
+        ans=0
+        for i in range(len(nums)):
+            s=nums[i]+prefix[i]
+            ans=ans+s
+            res.append(ans)
+        return res
