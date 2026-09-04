d17b2d9f6a2778136ac30b40b60bd785425f86c2
Arish Muthaiah.K J <aswinharish_bcs27@mepcoeng.ac.in>
2026-09-04 08:11:54 +0530

    Time: 0 ms (100%), Space: 34 MB (13.14%) - LeetHub

--- 2091-removing-minimum-and-maximum-from-array/2091-removing-minimum-and-maximum-from-array.py
+++ 2091-removing-minimum-and-maximum-from-array/2091-removing-minimum-and-maximum-from-array.py
@@ -4,8 +4,7 @@ class Solution:
         max_idx=nums.index(max(nums))
         back1=len(nums)-min_idx
         back2=(len(nums)-max_idx)
-        print(min_idx,max_idx)
-        print(back1,back2)
+     
         res1=min(min_idx+1,back1)
         res2=min(max_idx+1,back2)
         return min(res1+res2,max(min_idx+1,max_idx+1),max(back1,back2))
